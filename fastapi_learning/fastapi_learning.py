dd3d623254096f64a8c68b1520d96f67b8e2db25
Derek Wan <d.wan@icloud.com>
2021-01-06 22:18:48 +0800

    Add read_user_item

--- fastapi_learning/fastapi_learning.py
+++ fastapi_learning/fastapi_learning.py
@@ -83,6 +83,25 @@ async def read_item_2(
     return out
 
 
+@_APP_GET("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int,
+    item_id: str,
+    q: Optional[str] = None,
+    short: bool = False,
+) -> Dict[str, Any]:
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "This is an amazing item that has a long description",
+            },
+        )
+    return item
+
+
 @_APP_PUT("/items/{item_id}")
 async def update_item(item_id: int, item: Item) -> Dict[str, Any]:
     """Update an item."""
