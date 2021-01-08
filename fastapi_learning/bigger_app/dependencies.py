ffe4891e32ff0fe8b58772e9848357a2702dcdec
Derek Wan <d.wan@icloud.com>
2021-01-08 23:24:32 +0800

    Use status_code from starlette

--- fastapi_learning/bigger_app/dependencies.py
+++ fastapi_learning/bigger_app/dependencies.py
@@ -1,12 +1,19 @@
 from fastapi import Header
 from fastapi import HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
 
 
 async def get_token_header(x_token: str = Header(...)) -> None:
     if x_token != "fake-super-secret-token":  # noqa:S105
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="X-Token header invalid",
+        )
 
 
 async def get_query_token(token: str) -> None:
     if token != "jessica":  # noqa:S105
-        raise HTTPException(status_code=400, detail="No Jessica token provided")
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="No Jessica token provided",
+        )
