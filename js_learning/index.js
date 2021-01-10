39d88a290f3fe1ff47882b31342261c780bfc82d
Derek Wan <d.wan@icloud.com>
2021-01-10 18:51:49 +0800

    Also run on TypeScript

--- js_learning/index.js
+++ js_learning/index.js
@@ -1,1 +1,21 @@
 console.log("foobarbaz");
+
+var number = 5;
+number - 9;
+var myName = "Beau";
+
+myName = 8;
+
+let ourName = "freeCodeCamp";
+
+const pi = 3.14;
+const pi2 = 1.4;
+
+var a;
+var b = 2;
+a = 7;
+b = a;
+
+console.log(a);
+console.log(b);
+console.log(a + b);
