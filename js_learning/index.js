87a0f2c5aff2e9f887c458275f31809cab7138de
Derek Wan <d.wan@icloud.com>
2021-01-11 11:19:58 +0800

    WIP

--- js_learning/index.js
+++ js_learning/index.js
@@ -1,21 +1,31 @@
-console.log("foobarbaz");
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  return (
+    "The " +
+    myAdjective +
+    " " +
+    myNoun +
+    " " +
+    myVerb +
+    " to the store " +
+    myAdverb
+  );
+}
+console.log("wordBlanks:", wordBlanks("cat", "black", "house", "quickly"));
 
-var number = 5;
-number - 9;
-var myName = "Beau";
+var testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+};
+console.log(testObj.hat);
+// console.log(testObj.hasOwnProperty("hat"));
+// console.log(testObj.hasOwnProperty("hattt"));
 
-myName = 8;
+const magic = () => new Date(); // an anonymous function
+console.log("magic:", magic());
 
-let ourName = "freeCodeCamp";
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log("myConcat:", myConcat([1, 2], [3, 4]));
 
-const pi = 3.14;
-const pi2 = 1.4;
-
-var a;
-var b = 2;
-a = 7;
-b = a;
-
-console.log(a);
-console.log(b);
-console.log(a + b);
+const squareOfPosInts = (arr) =>
+  arr.filter((x) => Number.isInteger(x) && x > 0).map((x) => x ** 2);
+console.log("squareOfPosInts:", squareOfPosInts([-1, 0, 1, 2, 3]));
